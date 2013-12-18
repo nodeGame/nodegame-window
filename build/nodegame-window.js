46ad1871949575b83070af7d9357fd5d9b7e24f7
Benedek Vartok <vartokb@student.ethz.ch>
2013-12-18 19:14:27 +0100

    Made lockScreen re-try with a timeout

--- build/nodegame-window.js
+++ build/nodegame-window.js
@@ -1523,6 +1523,9 @@
      * TODO: check if this can be called in any stage.
      */
     GameWindow.prototype.lockFrame = function(text) {
+        var that;
+        that = this;
+
         if (!this.waitScreen) {
             throw new Error('GameWindow.lockFrame: waitScreen not found.');
         }
@@ -1531,7 +1534,9 @@
                                 'or undefined');
         }
         if (!this.isReady()) {
-            throw new Error('GameWindow.lockFrame: window not ready.');
+console.log('*** GameWindow.lockFrame: SETTING TIMEOUT');
+            setTimeout(function() { that.lockFrame(text); }, 100);
+            //throw new Error('GameWindow.lockFrame: window not ready.');
         }
         this.setStateLevel('LOCKING');
         text = text || 'Screen locked. Please wait...';
