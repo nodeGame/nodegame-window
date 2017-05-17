7bd034bb89cc67d7b2a13f01f230bfd270e007e2
Michael Yang <themichaelyang@gmail.com>
2017-05-17 19:06:32 -0400

    added generate info panel method for window

--- build/nodegame-window.js
+++ build/nodegame-window.js
@@ -829,6 +829,11 @@
         // return info panel also add it to the html
         // create a reference inside game window
         // infoPanel (this)
+
+        this.infoPanel = new node.InfoPanel({});
+        document.body.appendChild(this.infoPanel.infoPanelDiv);
+
+        return this.infoPanel;
     };
 
     /**
@@ -3054,13 +3059,83 @@
     exports.InfoPanel = InfoPanel;
 
     function InfoPanel(options) {
-        this.init();
+        this.init(options || {});
     }
 
     InfoPanel.prototype.init = function(options) {
         this.infoPanelDiv = document.createElement('div');
         this.infoPanelDiv.id = 'ng_info-panel';
-        this.infoPanelDiv.className = options.className || '';
+
+        /**
+         * ### InfoPanel.className
+         *
+         * Class name of info panel
+         *
+         * Default: ''
+         */
+        if ('undefined' === typeof options.className) {
+            this.infoPanelDiv.className = '';
+        }
+        else if ('string' === typeof options.className) {
+            this.infoPanelDiv.className = options.className;
+        }
+        else {
+            throw new TypeError('InfoPanel constructor: options.className ' +
+                                'must be a string or undefined. ' +
+                                'Found: ' + options.className);
+        }
+
+        /**
+         * ### InfoPanel.isVisible
+         *
+         * Boolean indicating visibility of info panel div
+         *
+         * Default: false
+         */
+        if ('undefined' === typeof options.isVisible) {
+            this.isVisible = false;
+        }
+        else if ('boolean' === typeof options.isVisible) {
+            this.isVisible = options.isVisible;
+        }
+        else {
+            throw new TypeError('InfoPanel constructor: options.isVisible ' +
+                                'must be a boolean or undefined. ' +
+                                'Found: ' + options.isVisible);
+        }
+
+        if (!this.isVisible) {
+            this.infoPanelDiv.style.display = 'none';
+        }
+        else {
+            this.infoPanelDiv.style.display = 'block';
+        }
+
+        /**
+         * ### InfoPanel.clearPattern
+         *
+         * String indicating when Info Panel should automatically clear:
+         * either: 'STEP', 'STAGE', 'NONE'
+         * (after each step, after each stage, or entirely manually)
+         *
+         * Default: 'NONE'
+         */
+        if ('undefined' === typeof options.clearPattern) {
+            this.clearPattern = 'NONE';
+        }
+        else if ('string' === typeof options.clearPattern &&
+                ['STEP', 'STAGE', 'NONE'].reduce(function(acc, value) {
+                  return options.clearPattern === value;
+                }, false)) {
+            this.clearPattern = options.clearPattern;
+        }
+        else {
+            throw new TypeError('InfoPanel constructor: options.clearPattern ' +
+                                'must be string "STEP", "STAGE", "NONE", ' +
+                                'or undefined. ' +
+                                'Found: ' + options.clearPattern);
+        }
+
     };
 
     InfoPanel.prototype.clear = function() {
@@ -3086,19 +3161,44 @@
     };
 
     InfoPanel.prototype.toggle = function() {
+      this.isVisible = !this.isVisible;
 
+      console.log(this);
+
+      if (this.isVisible) {
+        this.open();
+      }
+      else {
+        this.close();
+      }
     };
 
     InfoPanel.prototype.open = function() {
-
+      this.infoPanelDiv.style.display = 'block';
+      this.isVisible = true;
     };
 
     InfoPanel.prototype.close = function() {
-
+      this.infoPanelDiv.style.display = 'none';
+      this.isVisible = false;
     };
 
     InfoPanel.prototype.createToggleButton = function() {
         // return a button that toggles info panel
+        var button;
+        var that;
+
+        that = this;
+
+        button = document.createElement('button');
+        button.className = 'btn btn-lg btn-warning';
+        button.innerHTML = 'History';
+
+        button.onclick = function() {
+          that.toggle();
+        };
+
+        return button;
     };
 
 })(
